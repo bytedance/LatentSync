b687df21b19752e4c7e9834aae05a664aab8c418
Luca Saggese <luca.saggese@gmail.com>
2025-04-30 14:30:14 +0200

    wip

--- build_docker.sh
+++ build_docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-docker build -t latentsync .
+docker build -t lsaggese/ai_latentsync .
+docker push lsaggese/ai_latentsync
 echo "✅ Build completata!"
 echo "👉 Per eseguire il container usa:"
-echo "docker run --rm -it --gpus all -p 8082:8080  latentsync"
+echo "docker run --rm -it --gpus all -p 8082:8080  lsaggese/ai_latentsync"
